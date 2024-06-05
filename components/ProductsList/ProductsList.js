b3fcac5ecc9c3e5ec19d1be8aea78acb8080c666
berfinbsc <berfinbasci4701@gmail.com>
2024-06-06 01:06:55 +0200

    filter products

--- components/ProductsList/ProductsList.js
+++ components/ProductsList/ProductsList.js
@@ -1,42 +1,55 @@
 "use client";
-import React from "react";
-import { useEffect, useState } from "react";
-import styles from '../FilterMenu.module.css'
+import React, { useEffect, useState } from "react";
+import styles from '../FilterMenu.module.css';
 import ProductCard from "./ProductCard";
+import filterStore from "@/utils/filterStore";
+
 export default function ProductsList() {
+  const [products, setProducts] = useState([]);
+  const { selectedFilters } = filterStore();
+  const [currentProducts, setCurrentProducts] = useState([]);
 
-const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
+        if (!response.ok) {
+          throw new Error('API hatası: ' + response.status);
+        }
+        const data = await response.json();
+        setProducts(data.data);
+        setCurrentProducts(data.data);
+      } catch (error) {
+        console.error('Veri çekme hatası: ', error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
-            if (!response.ok) {
-              throw new Error('API hatası: ' + response.status);
-            }
-            const data = await response.json();
-            setProducts(data.data);
-          } catch (error) {
-            console.error('Veri çekme hatası: ', error);
+  useEffect(() => {
+    if (selectedFilters && products.length > 0) {
+      const filteredProducts = products.filter(product => {
+        return selectedFilters.some(filter => {
+          if (filter.name === product.STKOZKOD3) {
+            return filter.categories.includes(product.STKOZKOD5);
           }
-        };
-        fetchData();
-      }, []);
-    
-
-
-
-
-
+          return false;
+        });
+      });
+      setCurrentProducts(filteredProducts.length > 0 ? filteredProducts : products);
+    } else {
+      setCurrentProducts(products);
+    }
+  }, [selectedFilters, products]);
 
   return (
     <div className={styles.productList}>
-      {products.map((product, index) => (
+      {currentProducts.map((product, index) => (
         <div key={index} className={styles.productCard}>
           <ProductCard product={product} />
         </div>
       ))}
     </div>
-  )
+  );
 }
