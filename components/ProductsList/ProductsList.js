a962d2acc278195f8745f51e5aabee911e163a9c
berfinbsc <berfinbasci4701@gmail.com>
2024-06-10 15:26:41 +0200

    update

--- components/ProductsList/ProductsList.js
+++ components/ProductsList/ProductsList.js
@@ -12,7 +12,7 @@ export default function ProductsList() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_VERCEL}/products`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
         if (!response.ok) {
           throw new Error('API hatası: ' + response.status);
         }
